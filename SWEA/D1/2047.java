d2021d2712a2616487b8f19388cffc3fd79c4652
hyunji <phj9573@naver.com>
2024-10-29 01:13:01 +0900

    [D1] 신문 헤드라인

---
+++ SWEA/D1/2047.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		
+		Scanner sc = new Scanner(System.in);
+		String T;
+		T=sc.next();
+		
+
+		T= T.toUpperCase();
+        System.out.print(T);
+	}
+}
